3d94b4aaae1914225829ad70b0b7e2eda8a7b8da
DrTeePot <tristanpotter@rogers.com>
2014-02-13 16:52:57 -0500

    Large Update
    Completed all content! YAY!
    Altered structure, moved stylesheets into one folder.
    Created a more logical structure for blogs.
    Updated web_resources with some new pics

---
+++ post.py
@@ -0,0 +1,15 @@
+## Tristan Potter
+## This allows for a user to post to the blog. It will modify
+##     the blog.html file while creating a new file in the blog folder
+##     that cooresponds to the post.
+
+int postNumber
+
+blog = open("blog.html", "r")
+new_blog = open("new_blog.html", "w")
+
+def findBlogNumber()
+
+    blog.
+
+file
